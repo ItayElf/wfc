7948dafab794d1edbc59e068dc7e5045c5b79468
ItayElf <thh123321@gmail.com>
2023-09-12 21:32:45 +0300

    Feat: added collapse at

--- src/wfc/wfc/algorithm.rs
+++ src/wfc/wfc/algorithm.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+use rand::seq::SliceRandom;
+
 type WfcVector = Vec<HashSet<String>>;
 
 /// Returns wether the vector is collapsed
@@ -10,6 +12,26 @@ pub fn is_collapsed(wfc_vector: &WfcVector) -> bool {
         }
     }
     true
+}
+
+/// Collapse the set at the given location, returning the value
+fn collapse_at(wfc_vector: &mut WfcVector, index: usize) -> Result<String, &'static str> {
+    if index >= wfc_vector.len() {
+        return Err("Index was out of range");
+    }
+    let set = &wfc_vector[index];
+    let set_as_vec: Vec<&String> = set.iter().collect();
+
+    let value = set_as_vec.choose(&mut rand::thread_rng());
+
+    if value.is_none() {
+        return Err("Set was empty");
+    }
+
+    let value = String::from(*value.unwrap());
+    wfc_vector[index] = vec![value.clone()].into_iter().collect();
+
+    Ok(value)
 }
 
 /// Converts a WfcVector to a vector of strings.
@@ -28,7 +50,7 @@ pub fn flatten_wfc_vector(wfc_vector: WfcVector) -> Result<Vec<String>, &'static
 
 #[cfg(test)]
 mod tests {
-    use crate::wfc::wfc::algorithm::is_collapsed;
+    use crate::wfc::wfc::algorithm::{collapse_at, is_collapsed};
 
     use super::{flatten_wfc_vector, WfcVector};
 
@@ -44,6 +66,43 @@ mod tests {
         vector[0].insert(String::new());
 
         assert_eq!(is_collapsed(&vector), false);
+    }
+
+    #[test]
+    fn test_collapse_at_sanity() {
+        let mut vector: WfcVector = vec![
+            vec![
+                String::from("hello"),
+                String::from("world"),
+                String::from("test"),
+            ]
+            .into_iter()
+            .collect(),
+            vec![String::from("world")].into_iter().collect(),
+        ];
+        assert_ne!(vector[0].len(), 1);
+
+        collapse_at(&mut vector, 0).unwrap();
+
+        assert_eq!(vector[0].len(), 1);
+    }
+
+    #[test]
+    fn test_collapse_at_empty_set() {
+        let mut vector: WfcVector = vec![vec![].into_iter().collect()];
+
+        let result = collapse_at(&mut vector, 0);
+
+        assert_eq!(result, Err("Set was empty"));
+    }
+
+    #[test]
+    fn test_collapse_at_invalid_index() {
+        let mut vector: WfcVector = vec![];
+
+        let result = collapse_at(&mut vector, 100);
+
+        assert_eq!(result, Err("Index was out of range"));
     }
 
     #[test]
